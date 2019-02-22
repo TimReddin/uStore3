59f6c7bb4834ddb7e51ed33f97ff8268abcd79d6
Tim Reddin <timreddin@outlook.com>
2019-02-22 16:31:22 -0600

    Initial push

---
+++ uStore3.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="uStore3.UI.MVC.MvcApplication" Language="C#" %>
---
+++ uStore3.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uStore3.UI.MVC.Startup))]
+namespace uStore3.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
